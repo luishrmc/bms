7d95fd015ec4922438ff2af0846725c79796af5a
Luis Maciel <luis@logpyx.com>
2025-07-16 10:28:02 -0300

    adding paho-mqtt for c++

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <nlohmann/json.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    std::cout << "JSON: "
+              << NLOHMANN_JSON_VERSION_MAJOR << '.'
+              << NLOHMANN_JSON_VERSION_MINOR << '.'
+              << NLOHMANN_JSON_VERSION_PATCH << '\n';
+
     return 0;
 }
