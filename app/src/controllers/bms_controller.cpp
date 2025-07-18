ad62bb3c1c61fc7d41d22a85fbeda8aa0d3d4366
Luis Maciel <luishenrique.8804@gmail.com>
2025-07-18 15:43:45 -0300

    basic configuration - starting the development

---
+++ app/src/controllers/bms_controller.cpp
@@ -0,0 +1,39 @@
+/**
+ * @file        bms_controller.cpp
+ * @author      Luis Maciel (luishrm@ufmg.br)
+ * @brief       [Short description of the file’s purpose]
+ * @version     0.0.1
+ * @date        YYYY-MM-DD
+ *               _   _  _____  __  __   _____
+ *              | | | ||  ___||  \/  | / ____|
+ *              | | | || |_   | \  / || |  __
+ *              | | | ||  _|  | |\/| || | |_ |
+ *              | |_| || |    | |  | || |__| |
+ *               \___/ |_|    |_|  |_| \_____|
+ *
+ *            Universidade Federal de Minas Gerais
+ *                DELT · BMS Project
+ */
+
+// ----------------------------- Includes ----------------------------- //
+#include <iostream>
+#include "config.hpp"
+
+// -------------------------- Private Types --------------------------- //
+
+// -------------------------- Private Defines -------------------------- //
+
+// -------------------------- Private Macros --------------------------- //
+
+// ------------------------ Private Variables -------------------------- //
+
+// ---------------------- Function Prototypes -------------------------- //
+
+// ------------------------- Main Functions ---------------------------- //
+int main()
+{
+    std::cout << "[Main] Starting application: " << project_name << " v" << project_version << "\n";
+    return 0;
+}
+
+// *********************** END OF FILE ******************************* //
---
+++ utils/pattern/file_pattern.hpp
@@ -0,0 +1,33 @@
+/**
+ * @file        filename.hpp
+ * @author      Luis Maciel (luishrm@ufmg.br)
+ * @brief       [Short description of the file’s purpose]
+ * @version     0.0.1
+ * @date        YYYY-MM-DD
+ *               _   _  _____  __  __   _____ 
+ *              | | | ||  ___||  \/  | / ____|
+ *              | | | || |_   | \  / || |  __
+ *              | | | ||  _|  | |\/| || | |_ |
+ *              | |_| || |    | |  | || |__| |
+ *               \___/ |_|    |_|  |_| \_____|      
+ * 
+ *            Universidade Federal de Minas Gerais
+ *                DELT · BMS Project
+ */
+
+#ifndef FILENAME_HPP
+#define FILENAME_HPP
+
+// ----------------------------- Includes ----------------------------- //
+
+// -------------------------- Public Types ---------------------------- //
+
+// -------------------------- Public Defines -------------------------- //
+
+// -------------------------- Public Macros --------------------------- //
+
+// ------------------------ Public Functions -------------------------- //
+
+#endif // FILENAME_HPP
+
+// *********************** END OF FILE ******************************* //
